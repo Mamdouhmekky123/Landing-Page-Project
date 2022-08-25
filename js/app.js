cb31e59391cd3b36b8ad3690ce5f00efcb855a97
Mamdouhmekky123 <84875087+Mamdouhmekky123@users.noreply.github.com>
2022-08-25 18:53:12 +0200

    new updates

--- js/app.js
+++ js/app.js
@@ -26,32 +26,32 @@
 const nav = document.getElementById('navbar__list');
 // sections global var
 const sec = document.querySelectorAll('section');
-
-
+//style bar for shrinking
+const shrinkIcon = document.querySelector(".navbar_shrink");
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-function navegationBuilder  ()  {
+function navegationBuilder() {
 
-    nav.innerHTML = "";
-    // looping all over the sections
-   for(section of sec) {
+  nav.innerHTML = "";
+  // looping all over the sections
+  for (section of sec) {
 
-        const sectionID = section.id;
-        const sectionDataNav = section.dataset.nav;  //I wanted to call custom  attribute called (nav)
+    const sectionID = section.id;
+    const sectionDataNav = section.dataset.nav;  //I wanted to call custom  attribute called (nav)
 
-        const items = document.createElement('li');
+    const items = document.createElement('li');
 
-      items.innerHTML=`<a class="menu__link" data-nav="${sectionID}" href="#${sectionID}">${sectionDataNav}</a>`;
-       
-        nav.appendChild(items);
+    items.innerHTML = `<a class="menu__link" data-nav="${sectionID}" href="#${sectionID}">${sectionDataNav}</a>`;
 
-         //nav.insertAdjacentElement("beforeend",items);    (I can use it too)
-         
-    };
+    nav.appendChild(items);
+
+    //nav.insertAdjacentElement("beforeend",items);    (I can use it too)
+
+  };
 
 };
 
@@ -69,40 +69,67 @@ navegationBuilder();
 
 // Add class 'active' to section when near top of viewport
 
-
+/*
 //Activat the section which is viewed
-function viewActiveSection (part) {
-    return part.getBoundingClientRect().top;
+function viewActiveSection(part) {
+  return part.getBoundingClientRect().top;
 };
-
+*/
 
 
 //implementating the actual function
 
+//position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)
 
+function addAndRemoveActiveClass() {
+  const navItems = document.querySelectorAll('#navbar__list a');
+  navItems.forEach(item => {
 
-function addAndRemoveActiveClass  () {
-    const navItems = document.querySelectorAll('#navbar__list a');
-    navItems.forEach(item =>{
+    if (!item.hash) return;
 
-      if (! item.hash) return;
+    let section = document.querySelector(item.hash);
 
-      let  section=document.querySelector(item.hash);
+    let position = window.scrollY;
 
-      let position= window.scrollY+150;
+    if (position >= section.offsetTop - (section.offsetHeight * .35) &&
+      position <
+      section.offsetTop + section.offsetHeight - (section.offsetHeight * .35)) {
+      item.classList.add('your-active-class');
+    }
+    else {
+      item.classList.remove('your-active-class');
+    }
 
-      if (position>= section.offsetTop && position <= (section.offsetTop + section.offsetHeight))
-      {
-        item.classList.add('active');
+  }),
+    navItems.forEach(item => {
+
+      if (!item.hash) return;
+
+      let section = document.querySelector(item.hash);
+
+      let position = window.scrollY;
+
+      if (position >= section.offsetTop - (section.offsetHeight * .35) &&
+        position <
+        section.offsetTop + section.offsetHeight - (section.offsetHeight * .35)) {
+        section.classList.add('your-active-class');
       }
-      else
-      {
-        item.classList.remove('active');
+      else {
+        section.classList.remove('your-active-class');
       }
-    })
- 
-};window.addEventListener('load' ,addAndRemoveActiveClass);
-window.addEventListener('scroll' ,addAndRemoveActiveClass);
+
+    }
+    )
+
+};
+
+shrinkIcon.addEventListener("click", function shrink(part) {
+  nav.classList.toggle("responsive");
+  part.preventDefault();
+});
+
+window.addEventListener('load', addAndRemoveActiveClass);
+window.addEventListener('scroll', addAndRemoveActiveClass);
 
 /**
  * End Main Functions
@@ -117,15 +144,15 @@ window.addEventListener('scroll' ,addAndRemoveActiveClass);
 //  when click go smoothly (scrolling) to the target we want (custom attribute (sectionID))
 
 nav.addEventListener("click", (action) => {
-    action.preventDefault();
-    if (action.target.dataset.nav) {
-      document.getElementById(`${action.target.dataset.nav}`).scrollIntoView({ behavior: "smooth" });
+  action.preventDefault();
+  if (action.target.dataset.nav) {
+    document.getElementById(`${action.target.dataset.nav}`).scrollIntoView({ behavior: "smooth" });
 
-      setTimeout(() => {
-        location.hash = `${action.target.dataset.nav}`;
-      }, 155);
-    }
-  });
+    setTimeout(() => {
+      location.hash = `${action.target.dataset.nav}`;
+    }, 155);
+  }
+});
 
 
 
